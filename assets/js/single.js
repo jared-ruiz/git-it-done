6d652a6449411e20192c91ef9d29f4e0d8f6890e
Jared Ruiz <jared707@gmail.com>
2022-03-11 04:11:08 -0600

    Added link to repos with more than 30 issues.

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 //querySelectors
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 //fetch request for repo issues
 var getRepoIssues = function(repo) {
@@ -9,9 +10,14 @@ var getRepoIssues = function(repo) {
         //request was successful
         if (response.ok) {
             response.json().then(function(data) { 
-                console.log(data);
+                
                 //pass response data to dom function
                 displayIssues(data);
+
+                //check if api has paginated issues (more than 30 that can display)
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -68,5 +74,19 @@ var displayIssues = function(issues) {
 
 }
 
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    //append link element with an href attr that points to main github issue page
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
+
 //pass {owner}/{repo name}
-getRepoIssues("jared-ruiz/git-it-done");
+getRepoIssues("facebook/react");
